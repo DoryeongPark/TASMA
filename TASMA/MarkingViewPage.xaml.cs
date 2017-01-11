503153949a3e7ee1ced34f8ecc0e402f75dea14a
Doryeong Park <zzada0520@gmail.com>
2017-01-12 02:18:20 +0900

    MarkingViewPage is added

---
+++ TASMA/MarkingViewPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TASMA.Database;
+
+namespace TASMA
+{
+    /// <summary>
+    /// 학생 채점 테이블 페이지 입니다.
+    /// </summary>
+    public partial class MarkingViewPage : Page, INotifyPropertyChanged
+    {
+        private AdminDAO adminDAO;
+
+        private string gradeName;
+        private string className;
+        private string subjectName;
+
+        private DataTable scoreTable;
+        public DataTable ScoreTable
+        {
+            get { return scoreTable; }
+            set { scoreTable = value; OnPropertyChanged("ScoreTable"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MarkingViewPage(AdminDAO adminDAO, string gradeName, string className, string subjectName)
+        {
+            this.adminDAO = adminDAO;
+            this.gradeName = gradeName;
+            this.className = className;
+            this.subjectName = subjectName;
+
+            
+
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
